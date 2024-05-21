db5f0eeb051e8517ae31d78f3f32ba81d886bbb3
rkran <rkranjith03@gmail.com>
2024-05-21 22:55:34 +0530

    cors

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from textblob import TextBlob
 import requests
 from bs4 import BeautifulSoup
 from googlesearch import search
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 def analyze_sentiment(text):
     analysis = TextBlob(text)
